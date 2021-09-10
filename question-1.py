fa0cfc1095b8b0c07e6455d6d01bd85be2dc8792
ManavPatni <87895188+ManavPatni@users.noreply.github.com>
2021-09-10 11:25:01 +0530

    Add files via upload

---
+++ question-1.py
@@ -0,0 +1,11 @@
+# Created by Manav Patni
+# Program to create Studentes Profile
+
+name = input("Enter your name here:- ")
+sid = int(input("Enter your sid here:- "))
+age = int(input("Enter your age here:- "))
+Class = input("Enter your class here:- ")
+pers = float(input("Enter persentage here:- "))
+
+print("Student Name", "\t", name, "\n", "Sid", "\t", sid, "\n", "Age",
+      "\t", age, "\n", "Class", "\t", Class, "\n", "Persentage", "\t", pers,"%")
